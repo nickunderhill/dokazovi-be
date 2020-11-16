5e401e3ef1ebf6a4b7c31e898dc9f692c744cf06
Nick Underhill <m.podopryhora@gmail.com>
2020-11-16 14:24:45 +0200

    test code

--- src/main/java/com/softserveinc/dokazovi/service/PostService.java
+++ src/main/java/com/softserveinc/dokazovi/service/PostService.java
@@ -2,13 +2,19 @@ package com.softserveinc.dokazovi.service;
 
 import com.softserveinc.dokazovi.dto.post.ImportantPostDTO;
 import com.softserveinc.dokazovi.dto.post.LatestPostDTO;
+import com.softserveinc.dokazovi.entity.PostEntity;
 import com.softserveinc.dokazovi.entity.enumerations.PostStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface PostService {
 
 	Page<LatestPostDTO> findAllByStatus(PostStatus postStatus, Pageable pageable);
 
 	Page<ImportantPostDTO> findImportantPosts(Pageable pageable);
+
+	List<PostEntity> findAll(Pageable pageable);
+
 }
